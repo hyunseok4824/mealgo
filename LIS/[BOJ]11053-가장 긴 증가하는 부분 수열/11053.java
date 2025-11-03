1cd6d114454e0e455ba8bf36bc089c22eee7e6f7
jgm00 <132114851+jgm00@users.noreply.github.com>
2025-11-03 21:49:22 +0900

    BOJ11053번 - 가장 긴 증가하는 부분 수열

---
+++ LIS/[BOJ]11053-가장 긴 증가하는 부분 수열/11053.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    static int N, ans;
+    static int[] num, cnt;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        num = new int[N]; cnt = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            num[i] = Integer.parseInt(st.nextToken());
+            int maxValue = 0;
+            for(int j=0;j<i;j++){
+                if(num[i] > num[j] && maxValue < cnt[j]){
+                    maxValue = cnt[j];
+                }
+            }
+            cnt[i] = maxValue + 1;
+            ans = Math.max(cnt[i],ans);
+        }
+        System.out.println(ans);
+    }
+}
